b8e66a44d8f0bd38065e0f247be8e2020042aa8c
emil frisk <emilnygaardfrisk@gmail.com>
2016-04-11 19:14:45 +0200

    testing gitignore

--- heapsort/Heapsort.java
+++ heapsort/Heapsort.java
@@ -4,10 +4,5 @@ public class Heapsort {
     public Heapsort() {}
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner();
-        String[] input = scan.nextLine().split(" ");
-        PQHeap heap = new PQHeap(input.length);
-        
-        System.out.println(Arrays.toString(input));
     }
 }
