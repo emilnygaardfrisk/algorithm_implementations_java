d35f3a9360c4098ec5acc3599206b0dd557c3a9d
emil frisk <emilnygaardfrisk@gmail.com>
2016-04-11 23:38:24 +0200

    heapsort works, needs testing

--- heapsort/Heapsort.java
+++ heapsort/Heapsort.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Heapsort {
-    public Heapsort() {}
+
+    public static int[] sort(int[] a) {
+        PQHeap h = new PQHeap(a.length);
+        for (int i : a) h.insert(new Element(i, null)); 
+        for (int i = 0; i < a.length; i++) a[i] = h.extractMin().key;
+        return a;
+    }
 
     public static void main(String[] args) {
+        int[] a = {3,8,2,1,31,5,23,3,6};
+        System.out.println(Arrays.toString(Heapsort.sort(a)));
     }
 }
